204c844c55f02180589973056d9322a9c4ff2bb5
root <aftechiphone@gmail.com>
2018-08-09 18:27:49 +0530

    docker commit

---
+++ services/users/src/routes/users.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const localAuth = require('../auth/local');
+const authHelpers = require('../auth/_helpers');
+
+const router = express.Router();
+
+router.get('/ping', (req, res) => {
+  res.send('pong');
+});
+
+router.post('/register', (req, res) => {
+  return authHelpers.createUser(req, res)
+  .then((user) => { return localAuth.encodeToken(user[0]); })
+    .then((token) => {
+    res.status(200).json({
+      status: 'success',
+      token,
+    });
+  })
+  .catch(() => {
+    res.status(500).json({
+      status: 'error',
+    });
+  });
+});
+
+router.post('/login', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  var data= {token : '$2y$12$Ncryg5rk1Qq8oZZNIEOI/uTjcHewCBjzsI4fEN5S58C896zP7d41',user: 'michael'}
+  res.send(data);
+  // return authHelpers.getUser(username)
+  // .then((response) => {
+  //   if (!authHelpers.comparePass(password, response.password)) {
+  //     throw new Error('Incorrect password');
+  //   }
+  //   return response;
+  // })
+  // .then((response) => { return localAuth.encodeToken(response); })
+  // .then((token) => {
+  //   res.status(200).json({
+  //     status: 'success',
+  //     token,
+  //   });
+  // })
+  // .catch((err) => {
+  //   res.status(500).json({
+  //     status: 'error',
+  //     message: err,
+  //   });
+  // });
+});
+
+router.get('/user', authHelpers.ensureAuthenticated, (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    user: req.user,
+  });
+});
+
+module.exports = router;
